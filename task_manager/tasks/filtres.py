d436d5b565e5594bf0a7fae6024b88a730f63d16
Валентин Алтабасов <valenjir@gmail.com>
2025-11-03 14:32:53 +0300

    step 7

--- task_manager/tasks/filtres.py
+++ task_manager/tasks/filtres.py
@@ -1,15 +1,36 @@
 
 
 import django_filters as df
-
+from django import forms
 from .models import Task , Labels , Status, User
 
 
 class TaskFilter(df.FilterSet):
-    labels = df.ModelChoiceFilter(queryset=Labels.objects.all())
-    status = df.ModelChoiceFilter(queryset=Status.objects.all())
-    executor = df.ModelChoiceFilter(queryset=User.objects.all())
+    labels = df.ModelChoiceFilter(
+        queryset=Labels.objects.all(),
+        lebels='Метка',
+        widget=forms.Select(attrs={'class': 'form-select'}))
+    status = df.ModelChoiceFilter(
+        queryset=Status.objects.all(),
+        labels='Статус',
+        widget=forms.Select(attrs={'class':'form-select'}))
+    executor = df.ModelChoiceFilter(
+        queryset=User.objects.all(),
+        label='Исполнитель',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+    self_tasks = df.BooleanFilter(
+        label='Только свои задачи',
+        method='filter_self_tasks',
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
 
     class Meta:
         model = Task
-        fields = ['name', 'status', 'executor', 'author', 'created_at']
+        fields = ['status', 'executor', 'labels']
+
+    def filters_self_tasks(self, queryset, name, value):
+        if value:
+            user = self.request.user
+            return queryset.filter(author=user)
+        return queryset
