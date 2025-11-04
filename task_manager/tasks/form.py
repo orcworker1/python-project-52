063aa192e0ad7a34850bb4b02b5cada39daa41ec
Валентин Алтабасов <valenjir@gmail.com>
2025-11-04 17:07:59 +0300

    fix test

--- task_manager/tasks/form.py
+++ task_manager/tasks/form.py
@@ -1,10 +1,15 @@
 from django import forms
 
 from .models import Task
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class TaskForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Ensure executor options use usernames for labels as expected by tests
+        self.fields['executor'].queryset = User.objects.all()
+        self.fields['executor'].label_from_instance = lambda u: u.username
     class Meta:
         model = Task
         fields = ['name','description','status','executor','labels']
