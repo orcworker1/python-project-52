6252a6e3f544475ae33078ccd86897fd76f836ee
Валентин Алтабасов <valenjir@gmail.com>
2025-11-04 17:43:24 +0300

    fix test

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -6,8 +6,8 @@ from task_manager.tasks.views import ViewTasks, CreateTask, UpdateTask, DeleteTa
 urlpatterns = [
     path('', ViewTasks.as_view(), name='tasks'),
     path('create/',CreateTask.as_view(),name='create_task'),
-    path('<int:pk>/update', UpdateTask.as_view(),name='update_task'),
-    path('<int:pk>/delete>', DeleteTask.as_view(),name='delete_task'),
+    path('<int:pk>/update/', UpdateTask.as_view(),name='update_task'),
+    path('<int:pk>/delete/', DeleteTask.as_view(),name='delete_task'),
     path('<int:pk>/task', DetailViewTask.as_view(),name='detail_view'),
 ]
 # Create your views here.
