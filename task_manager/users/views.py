96a566ebd8b2aa7be88e712724171b6173e1791f
Валентин Алтабасов <valenjir@gmail.com>
2025-11-04 19:06:38 +0300

    fix test

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -9,16 +9,19 @@ from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from task_manager.tasks.models import Task
 
+
 class ViewUsers(ListView):
     model = User
     template_name = 'users/users_list.html'
     context_object_name = 'users'
+
 
 class CreateUser(CreateView):
     model = User
     template_name = 'users/create.html'
     form_class = CustomUserForm
     success_url = reverse_lazy('login')
+
     def form_valid(self, form):
         response = super().form_valid(form)
         messages.success(self.request, 'Пользователь успешно зарегистрирован')
@@ -29,11 +32,14 @@ class UserLoginView(LoginView):
     template_name = 'users/login.html'
     redirect_authenticated_user = True
     form_class = CustomAuthenticationForm
+
     def get_success_url(self):
         return reverse_lazy('index')
+
     def form_valid(self, form):
-        messages.success(self.request,'Вы залогинены')
+        messages.success(self.request, 'Вы залогинены')
         return super().form_valid(form)
+
 
 class UserLogoutView(LogoutView):
     http_method_names = ['get', 'post']
@@ -45,29 +51,40 @@ class UserLogoutView(LogoutView):
     def get_success_url(self):
         return reverse_lazy('index')
 
+
 class UserDelete(DeleteView):
     model = User
     template_name = 'users/delete_user.html'
     success_url = reverse_lazy('users')
-    
+
     def get(self, request, *args, **kwargs):
         self.object = self.get_object()
-        # Если пользователь используется в задачах, не показываем подтверждение
         in_use = (
-            hasattr(self.object, 'created_task') and self.object.created_task.exists()
-        ) or (
-            hasattr(self.object, 'executed_tasks') and self.object.executed_tasks.exists()
-        ) or Task.objects.filter(author=self.object).exists() or Task.objects.filter(executor=self.object).exists()
+                         hasattr(self.object, 'created_task') and self.object.created_task.exists()
+                 ) or (
+                         hasattr(self.object, 'executed_tasks') and self.object.executed_tasks.exists()
+                 ) or Task.objects.filter(author=self.object).exists() or Task.objects.filter(
+            executor=self.object).exists()
         if in_use:
             messages.error(request, 'Невозможно удалить пользователя, потому что он используется')
             return redirect('users')
-        # Иначе показываем страницу подтверждения даже если это сам пользователь
         return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        in_use = ((
+                 hasattr(self.object, 'created_task') and self.object.created_task.exists()
+                 ) or
+                  (hasattr(self.object, 'executed_tasks') and self.object.executed_tasks.exists()
+                 ) or Task.objects.filter(author=self.object).exists() or Task.objects.filter(
+            executor=self.object).exists())
+
+        if in_use:
+            messages.error(request, 'Невозможно удалить пользователя, потому что он используется')
+            return redirect('users')
+
         messages.success(request, 'Пользователь успешно удален')
         return super().post(request, *args, **kwargs)
-
 
 
 class UserUpdate(UpdateView):
