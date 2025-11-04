e3ec3282c942542ab7a4583da37c04a3c5194509
Валентин Алтабасов <valenjir@gmail.com>
2025-11-04 16:36:57 +0300

    fix test

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -5,7 +5,7 @@ from task_manager.labels.views import ViewLabels, CreateLabels , UpdateLabels, D
 
 urlpatterns = [
     path('', ViewLabels.as_view(), name='labels'),
-    path('created/', CreateLabels.as_view(), name='create_label'),
+    path('create/', CreateLabels.as_view(), name='create_label'),
     path('<int:pk>/update/', UpdateLabels.as_view(), name='update_label'),
     path('<int:pk>/delete/', DeleteLabels.as_view(), name='delete_label'),
 ]
