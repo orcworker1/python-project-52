c14a776e1e54831ea44540bc4d9971432056b5fe
Валентин Алтабасов <valenjir@gmail.com>
2025-11-05 14:49:08 +0300

    fix test

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,12 +1,11 @@
-from cProfile import label
-
+from django.conf import settings
 from django.db import models
 from task_manager.statuses.models import Status
 from django.contrib.auth.models import User
 from task_manager.labels.models import Labels
 import django_filters
 
-
+"""""
 class Task(models.Model):
     name = models.CharField(max_length=255, unique=True )
     description = models.CharField(blank=True, max_length=300)
@@ -19,6 +18,35 @@ class Task(models.Model):
         ordering = ['id']
     def __str__(self):
         return self.name
+"""""
+
+class Task(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    status = models.ForeignKey(Status, on_delete=models.PROTECT,
+                               related_name="tasks")
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="created_tasks",
+    )
+    executor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="executed_tasks",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    labels = models.ManyToManyField(Labels, related_name="labeled_tasks",
+                                    blank=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return self.name
+
 
 class TaskFilter(django_filters.Filter):
     pass
